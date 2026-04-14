9c55d563b6d6057dd3e030142aef3e784f24082f
AlePerez25 <alejp12@uci.edu>
2026-04-14 13:24:08 -0700

    prefabs
    Prefabs

---
+++ 3D horror game/Assets/Camera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _mouseSensitivity;
+    
+    
+    public GameObject cursorui;
+    private Transform _cameraTrans;
+    private float _rotationX;
+    private float _rotationY;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        _cameraTrans = Camera.main.transform;
+        cursorui.SetActive(true); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y");
+        _rotationY += mouseY * _mouseSensitivity;
+        _rotationY = Mathf.Clamp(_rotationY, -60.0f, 60.0f);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        _rotationX += mouseX * _mouseSensitivity;
+
+        _cameraTrans.localEulerAngles = new Vector3(-_rotationY, 0, 0);
+        transform.localEulerAngles = new Vector3(0, _rotationX, 0); 
+    }
+}
