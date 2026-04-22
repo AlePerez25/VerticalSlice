947f17ee99109401e19b9bd4976b37ff3a7337e0
AlePerez25 <alejp12@uci.edu>
2026-04-21 22:22:44 -0700

    Sky and lamp

--- 3D horror game/Assets/Camera.cs
+++ 3D horror game/Assets/Camera.cs
@@ -4,33 +4,19 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] private float _moveSpeed;
-    [SerializeField] private float _mouseSensitivity;
-    [SerializeField] private GameObject cursorui;
-    
-    
-    private Transform _cameraTrans;
-    private float _rotationX;
-    private float _rotationY;
 
-    void Start()
+    public GameObject luz;
+
+    public void Update()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        _cameraTrans = Camera.main.transform;
-        cursorui.SetActive(true); 
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            luz.SetActive(!luz.activeSelf);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float mouseY = Input.GetAxis("Mouse Y");
-        _rotationY += mouseY * _mouseSensitivity;
-        _rotationY = Mathf.Clamp(_rotationY, -60.0f, 60.0f);
+    
 
-        float mouseX = Input.GetAxis("Mouse X");
-        _rotationX += mouseX * _mouseSensitivity;
 
-        _cameraTrans.localEulerAngles = new Vector3(-_rotationY, 0, 0);
-        transform.localEulerAngles = new Vector3(0, _rotationX, 0); 
-    }
+
 }
