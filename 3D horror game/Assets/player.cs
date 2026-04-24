c3c226f5fb7ce9c7c8c1b18fb5a11f8de2301062
AlePerez25 <alejp12@uci.edu>
2026-04-24 11:46:16 -0700

    Pick items and health UI

---
+++ 3D horror game/Assets/player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class player : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _pointsText;
+    public GameObject cursorui;
+    private Transform _cameraTrans;
+    
+
+    public int health = 100;
+    public int Max = 100;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        _cameraTrans = Camera.main.transform;
+        cursorui.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = new Ray(_cameraTrans.position, _cameraTrans.forward);
+            // get information back from the raycast.
+            RaycastHit hit;
+
+            // From raycast unity documentation
+            // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Physics.Raycast.html
+            if (Physics.Raycast(ray, out hit, 5f))
+            {
+                ItemPickUp pickup = hit.collider.GetComponent<ItemPickUp>();
+
+                if (pickup != null)
+                {
+                    pickup.Pickup();
+                }
+            }
+        }
+
+        _pointsText.text = "health: " + health;
+
+    }
+
+    public void addhealth(int amount)
+    {
+        health += amount;
+
+        if (health > Max)
+        health = Max;
+
+        //_pointsText.text = "points: " + health;
+
+    }
+}
