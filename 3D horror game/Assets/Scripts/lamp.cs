3f73dd0d4504267f7dbc834a852e39747ec5076e
AlePerez25 <alejp12@uci.edu>
2026-05-05 19:38:24 -0700

    particle system for mushrooms

--- 3D horror game/Assets/Scripts/lamp.cs
+++ 3D horror game/Assets/Scripts/lamp.cs
@@ -5,14 +5,35 @@ using UnityEngine;
 public class Lamp : MonoBehaviour
 {
 
-    public GameObject luz;
+    public GameObject luzBlanca;
+    public GameObject luzBlue;
+    public GameObject luzRed;
+    public GameObject luzYellow;
+
+
+
 
     public void Update()
     {   
         //turn on/off lamp with the same key in this case "Q"
         if(Input.GetKeyDown(KeyCode.Q))
         {
-            luz.SetActive(!luz.activeSelf);
+            luzBlanca.SetActive(!luzBlanca.activeSelf);
+        }
+
+        /*if(Input.GetKeyDown(KeyCode.W))
+        {
+            luzBlue.SetActive(!luzBlue.activeSelf);
+        }*/
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            luzRed.SetActive(!luzRed.activeSelf);
+        }
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            luzYellow.SetActive(!luzYellow.activeSelf);
         }
     }
 
